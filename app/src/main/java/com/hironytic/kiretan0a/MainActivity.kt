26210a4cf89a515ec09ec73bc7ee7d2c2f479c93
Hironori Ichimiya <hiron@hironytic.com>
2018-01-24 21:40:20 +0900

    Initial commit

---
+++ app/src/main/java/com/hironytic/kiretan0a/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hironytic.kiretan0a
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
